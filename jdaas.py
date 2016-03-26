fc2bcf63614c0c01549c048a976d052ac494a536
arlegault <alexlegault89@gmail.com>
2016-03-26 14:50:43 -0700

    Adding jdass and news changs that havent been commited

---
+++ jdaas.py
@@ -0,0 +1,45 @@
+import csv
+import random
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+def readCSVtoList(csvname, arrayname):
+    with open(csvname, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter = ',')
+        for row in reader:
+            arrayname.append(row)
+
+@app.route('/jokes', methods=['GET'])
+def JdJokes():
+
+    joke_array = []
+    readCSVtoList('jeffdeanjokes.csv', joke_array)
+
+    return random.choice(joke_array)[0]
+
+
+
+@app.route('/advice', methods=['GET'])
+def JeffAdvice():
+
+    advice_array = []
+    readCSVtoList('jeffdeanadvice.csv', advice_array)
+
+    return random.choice(advice_array)[0]
+
+
+
+@app.route('/numbers', methods=['GET'])
+def numbersEveryEngineerShouldKnow():
+
+    numbers_array = []
+    readCSVtoList('importantnumbers.csv', numbers_array)
+
+    return random.choice(numbers_array)[0]
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
